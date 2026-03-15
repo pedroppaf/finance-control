c934405fa38d6fbea34b53747a981560a0e4f9c6
Pedro <pedroppaf@gmail.com>
2026-03-15 14:33:14 -0300

    commit inicial

---
+++ src/test/java/com/pedro/finance_control/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.finance_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
