a3c6aeed689d9dd6a0a7acbce246428792adaa5f
Pedro <pedroppaf@gmail.com>
2026-04-30 13:33:40 -0300

    adicionado status code, cors configuravel e exception com logging

--- src/main/java/com/pedro/finance_control/controller/AuthController.java
+++ src/main/java/com/pedro/finance_control/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.pedro.finance_control.dto.auth.AuthResponse;
 import com.pedro.finance_control.dto.auth.LoginRequest;
 import com.pedro.finance_control.dto.auth.RegisterRequest;
 import com.pedro.finance_control.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,12 +23,22 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Register a new user", description = "Register a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User register successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request){
         AuthResponse response = authService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Login a user", description = "Login a new authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User login successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request){
         AuthResponse response = authService.login(request);
